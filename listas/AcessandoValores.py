3d7b131c439179a5105bb15f071c38a1ec5de76d
Diego Sena <diego.tjdosantos@gmail.com>
2022-03-13 23:13:56 -0300

    Iniciando com listas

---
+++ listas/AcessandoValores.py
@@ -0,0 +1,5 @@
+times = ["Santos", "Real Madrid", "Manchester United", "Milan"]
+
+print("Santos" in times)  # Retorna true se existe o valor procurado na lista
+print(times.index("Milan"))  # Retorna a posição do valor procurado
+print(f'Lista de times: {times}')  # Retorna os valores da lista
